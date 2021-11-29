ea43c4ca4f53e12b6955260c4717bbbf5cb0a596
kadu-v <kazuonsunday@yahoo.co.jp>
2021-11-30 01:44:59 +0900

    Fix: test case

--- src/test/scala/core/CoreSpec.scala
+++ src/test/scala/core/CoreSpec.scala
@@ -5,13 +5,6 @@ import org.scalatest._
 import chisel3._
 
 class BlinkSpec extends FlatSpec with ChiselScalatestTester with Matchers {
-  behavior of "LED"
-  it should "Blink" in {
-    test(new Blink(4)) { c =>
-      c.clock.step(16)
-      c.io.out.expect(true.B)
-      c.clock.step(16)
-      c.io.out.expect(false.B)
-    }
-  }
+  behavior of "HackCPU"
+
 }
