e5465143104c9414e4bf5d6d330cac738ca7ca1d
kadu-v <kazuonsunday@yahoo.co.jp>
2022-04-01 02:19:12 +0900

    Update: Chisel3 version to 3.5.1

--- src/test/scala/hackcore/CoreSpec.scala
+++ src/test/scala/hackcore/CoreSpec.scala
@@ -1,11 +1,15 @@
 package hackcore
 
 import chiseltest._
-import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 import chisel3._
 import top.Top
 
-class HackCoreSpec extends FlatSpec with ChiselScalatestTester with Matchers {
+class HackCoreSpec
+    extends AnyFlatSpec
+    with ChiselScalatestTester
+    with Matchers {
   behavior of "HackCPU"
   it should "test bin.hex" in {
     test(new Top("./hack/bin.hack")) { c =>
