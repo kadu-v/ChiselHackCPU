9f2f4127785474d73df635a1dc03339ce2beb005
kadu-v <kazuonsunday@yahoo.co.jp>
2022-04-22 02:25:49 +0900

    Add: test cases

--- src/test/scala/hackcore/CoreSpec.scala
+++ src/test/scala/hackcore/CoreSpec.scala
@@ -14,7 +14,7 @@ class HackCoreSpec
   behavior of "HackCPU"
 
   it should "test constants" in {
-    test(new Top("./hack/tests/Const/vm.hack"))
+    test(new Top("./hack/tests/Const/vm.hack", "./hack/init.bin"))
       .withAnnotations(Seq(WriteVcdAnnotation)) { c =>
         c.clock.step(500)
         c.io.debug.expect(15.asUInt())
@@ -22,18 +22,44 @@ class HackCoreSpec
   }
 
   it should "test sadd" in {
-    test(new Top("./hack/tests/sadd.hack"))
+    test(new Top("./hack/tests/sadd.hack", "./hack/init.bin"))
       .withAnnotations(Seq(WriteVcdAnnotation)) { c =>
         c.clock.step(500)
         c.io.debug2.expect(5.asUInt())
       }
   }
 
-  it should "test add" in {
-    test(new Top("./hack/tests/Add/vm.hack"))
+  it should "test add (8 + 8 = 16)" in {
+    test(new Top("./hack/tests/Add/vm.hack", "./hack/init.bin"))
       .withAnnotations(Seq(WriteVcdAnnotation)) { c =>
         c.clock.step(500)
         c.io.debug.expect(16.asUInt())
+      }
+  }
+
+  it should "test sub (8 - 7 = 1)" in {
+    test(new Top("./hack/tests/Sub/vm.hack", "./hack/init.bin"))
+      .withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+        c.clock.step(500)
+        c.io.debug.expect(1.asUInt())
+      }
+  }
+
+  it should "test fib(2) = 1 (fib(0) = 0, fib(1) = 1)" in {
+    test(new Top("./hack/tests/Fib2/vm.hack", "./hack/init.bin"))
+      .withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+        c.clock.setTimeout(0)
+        c.clock.step(1000)
+        c.io.debug.expect(1.asUInt())
+      }
+  }
+
+  it should "test fib(6) = 8 (fib(0) = 0, fib(1) = 1)" in {
+    test(new Top("./hack/tests/Fib6/vm.hack", "./hack/init.bin"))
+      .withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+        c.clock.setTimeout(0)
+        c.clock.step(5000)
+        c.io.debug.expect(8.asUInt())
       }
   }
 }
