d52f2f539b00fc1d0cee1e7e7e3775666efaa205
kadu-v <kazuonsunday@yahoo.co.jp>
2022-04-22 02:24:53 +0900

    Fix: pc load

--- src/main/scala/hackcore/PCLoad.scala
+++ src/main/scala/hackcore/PCLoad.scala
@@ -20,7 +20,15 @@ class PCLoad extends Module {
   val not_j2 = ~io.j2
   val not_j3 = ~io.j3
 
-  val x =
-    (not_j2 & io.j3 & not_zr & not_ng) | (not_j1 & io.j2 & io.zr & not_ng) | (io.j1 & io.j2 & io.j3) | (io.j1 & io.j2 & not_zr & io.ng) | (io.j1 & io.j2 & io.zr & io.ng)
-  io.load_pc := x & io.pc_flag
+  val gt_zero = io.pc_flag & not_j1 & not_j2 & io.j3 & not_zr & not_ng
+  val eq_zero = io.pc_flag & not_j1 & io.j2 & not_j3 & io.zr & not_ng
+  val ge_zero = io.pc_flag & not_j1 & io.j2 & io.j3 & io.zr & not_ng
+  val lt_zero = io.pc_flag & io.j1 & not_j2 & not_j3 & not_zr & io.ng
+  val ne_zero = io.pc_flag & io.j1 & not_j2 & io.j3 & not_zr
+  val le_zero = io.pc_flag & io.j1 & io.j2 & not_j3 & io.zr & io.ng
+  val jp_zero = io.pc_flag & io.j1 & io.j2 & io.j3
+  io.load_pc := gt_zero | eq_zero | ge_zero | lt_zero | ne_zero | le_zero | jp_zero
+  // val x =
+  // (not_j2 & io.j3 & not_zr & not_ng) | (not_j1 & io.j2 & io.zr & not_ng) | (io.j1 & io.j2 & io.j3) | (io.j1 & io.j2 & not_zr & io.ng) | (io.j1 & io.j2 & io.zr & io.ng)
+  // io.load_pc := x & io.pc_flag
 }
