904f60c0522501ce20bef8564e18d0159969c872
kadu-v <kazuonsunday@yahoo.co.jp>
2022-04-30 01:19:23 +0900

    Fix: MMIO

--- src/main/scala/mmio/MMIO.scala
+++ src/main/scala/mmio/MMIO.scala
@@ -2,7 +2,7 @@ package mmio
 
 import chisel3._
 import memory.RAM
-import uart.{Rx, Tx}
+import uart.Uart
 import chisel3.util.MuxCase
 import javax.swing.InputMap
 
@@ -26,7 +26,6 @@ class MMIO(init: String) extends Module {
 
     // Debug signal
     val debug = Output(UInt(16.W))
-    val rxdebug = Output(UInt(16.W))
   })
 
   /* Random Access Memory */
@@ -42,59 +41,29 @@ class MMIO(init: String) extends Module {
   )
 
   /* UART */
-  //  15       12  Tx       8  7       4   Rx        0
-  // |-----------------------------------------------|
-  // |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
-  // |-----------------------------------------------|
-  //            R          B         C           R B
-  //            U          U         L           E U
-  //            N          S         E           C S
-  //                       Y         A           I Y
-  //                                 R           E
-  //                                             V
-  //                                             E
-
-  // staus and controll register
-  val stCtlReg = RegInit(
-    VecInit(Seq.fill(16)(false.B))
+  val uart = Module(
+    new Uart(
+      8192, // address of status and controll register
+      8193, // address of RX
+      8194 // address of Tx
+    )
   )
+  uart.io.addrM := io.addrM
+  uart.io.inM := io.inM
+  uart.io.writeM := Mux(
+    io.writeM &&
+      (io.addrM === 8192.U
+        || io.addrM === 8193.U
+        || io.addrM === 8194.U),
+    true.B,
+    false.B
+  )
+  uart.io.rx := io.rx
+  io.rts := uart.io.rts
+  uart.io.cts := io.cts
+  io.tx := uart.io.tx
 
-  //  RX
-  val rx = Module(new Rx(12, 115200))
-  rx.io.rx := io.rx
-  io.rts := rx.io.rts
-  rx.io.cbf := stCtlReg(5)
-
-  // Tx
-  // buffer for Tx
-  val txBuff = RegInit(0.asUInt())
-  val tx = Module(new Tx(12, 115200))
-  tx.io.din := txBuff
-  tx.io.run := stCtlReg(12)
-  tx.io.cts := io.cts
-  io.tx := tx.io.tx
-
-  // status register
-  // Rx
-  stCtlReg(0) := rx.io.busy // busy flag
-  stCtlReg(1) := rx.io.recieved // recieved flag
-  // Tx
-  stCtlReg(8) := tx.io.busy
-
-  // controll register
-  when(io.addrM === 8192.asUInt() && io.writeM) {
-    // Rx
-    stCtlReg(5) := io.inM(5)
-    // Tx
-    stCtlReg(12) := io.inM(12)
-  }.elsewhen(io.addrM === 8194.asUInt() && io.writeM) {
-    txBuff := io.inM(7, 0)
-  }.otherwise {
-    // Rx
-    stCtlReg(5) := false.B
-    // Tx
-    stCtlReg(12) := false.B
-  }
+  /* SPI */
 
   /* Multiplexer */
   // if      addrM === 8192 then status and controll register of uart
@@ -104,9 +73,10 @@ class MMIO(init: String) extends Module {
   io.out := MuxCase(
     ram.io.out,
     Seq(
-      (io.addrM === 8192.asUInt()) -> stCtlReg.asUInt(),
-      (io.addrM === 8193.asUInt()) -> rx.io.dout,
-      (io.addrM === 8194.asUInt()) -> 0.asUInt()
+      (io.addrM === 8192.asUInt() || io.addrM === 8193
+        .asUInt() || io.addrM === 8194.asUInt()) -> uart.io.out
+        .asUInt()
+        .asUInt()
     )
   )
 
@@ -116,5 +86,4 @@ class MMIO(init: String) extends Module {
     debugReg := ram.io.out
   }
   io.debug := debugReg
-  io.rxdebug := rx.io.dout
 }
