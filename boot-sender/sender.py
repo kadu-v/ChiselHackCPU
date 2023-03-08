3fc800e18e704865626bfa00a302f733d6a15da4
kadu-v <kazuonsunday@yahoo.co.jp>
2023-03-08 17:40:36 +0900

    WIP

--- boot-sender/sender.py
+++ boot-sender/sender.py
@@ -15,14 +15,18 @@ _serial.rtscts = True
 # _serial.timeout = 3 #sec    z    
 cnt = 100
 i = pack('B', 98)
+
+lst = []
 for i in tqdm(range(cnt)):
-    time.sleep(0.2)
-    i = pack('B', 98)
-    print(i)
+    time.sleep(0.05)
+    i = pack('B', i)
     _serial.write(i)    
-    # result = _serial.read(1)
-    # print(result)
+    result = _serial.read(1)
+    lst.append(result)
 # time.sleep(1)
 # result = _serial.read(1)
 # print(result)
 _serial.close()
+
+for e in lst:
+    print(e)
